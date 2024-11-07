5cabf6b0115bc2923c5ddb4d38646c84522cd9d8
IT-Abteilung <it-abteilung@softengine.de>
2024-11-07 13:36:36 +0100

    Setup adapt

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Update des Systems und Installation der benötigten Pakete
 apt update
-apt install -y curl net-tools sudo nginx certbot python3 python3-full python3-python3-certbot-nginx
+apt install curl net-tools sudo nginx certbot python3 python3-full python3-python3-certbot-nginx  -y
 
 # Sicherstellen, dass NGINX Aktiv ist
 systemctl enable nginx
